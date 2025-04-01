8f736253ed228efe6a4f41851a948e454fe08236
Alejandro Correa Rodríguez <smxacorrea@gmail.com>
2025-04-01 12:56:51 +0200

    guardando modelos y ruta comment

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -3,10 +3,19 @@ const router = express.Router();
 const User = require(`../models/User.model`);
 const verifyToken = require("../middlewares/auth.middlewares")
 
+//Crear un comentario
+router.get
+
+
+//Editar un comentario
 
 
 
+// Todos los comentarios
 
+
+
+//Eliminar un comentario
 
 
 
